d4bb38469820a5da9bbc759a2883c512b62de661
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-18 23:13:17 +0800

    refactor(component): update the mode for simple text container

--- src/components/general/containers/simple_text.svelte
+++ src/components/general/containers/simple_text.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	export let otherItemprop = ""
+import { type Snippet } from "svelte"
 
-	export { otherItemprop as itemprop }
+let {
+	itemprop: otherItemprop = "",
+	children
+}: {
+	itemprop?: string
+	children: Snippet
+} = $props()
 
-	$: itemprop = otherItemprop === "" ? "text" : `${otherItemprop} text`
+let itemprop = $derived(otherItemprop === "" ? "text" : `${otherItemprop} text`)
 </script>
 
 <p {itemprop}>
-	<slot></slot>
+	{@render children()}
 </p>
