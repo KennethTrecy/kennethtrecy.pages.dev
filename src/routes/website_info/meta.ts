87ebd41fcfb5a241b7b0df893e915d196b8eb485
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 00:35:50 +0800

    seo(website info): draft the updated metadata

--- src/routes/website_info/meta.ts
+++ src/routes/website_info/meta.ts
@@ -6,7 +6,7 @@ const meta = definePageMeta("/website_info", {
 	"description": "This website's purpose, dependencies, and process are listed here",
 	"keywords": [ "about", "website" ],
 	"title": "About the Website",
-	"version": "1.001"
+	"version": "1.002-dev"
 })
 
 export default meta
