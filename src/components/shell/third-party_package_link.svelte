e98f8fc931ac9a62735d98315a3934369512b5cf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:40:40 +0800

    refactor(component): update the mode for third-party package link

--- src/components/shell/third-party_package_link.svelte
+++ src/components/shell/third-party_package_link.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import ExternalLink from "@/components/general/links/external.svelte"
+import ExternalLink from "@/components/general/links/external.svelte"
 
-	export let packageName: string
-	export let homeLink: string
-	export let licenseName: string
-	export let licenseLink: string
+let {
+	packageName,
+	homeLink,
+	licenseName,
+	licenseLink
+}: {
+	packageName: string
+	homeLink: string
+	licenseName: string
+	licenseLink: string
+} = $props()
 </script>
 
 <ExternalLink address={homeLink} itemprop="name">{packageName}</ExternalLink>
