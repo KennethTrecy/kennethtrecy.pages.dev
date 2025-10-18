ebc7faad4f721263b2240be93031086d089ce6bf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 00:43:27 +0800

    feat(constant): change the order of article collections

--- src/constants/article_meta_collection.ts
+++ src/constants/article_meta_collection.ts
@@ -1,15 +1,15 @@
 import { type ArticlePageMeta } from "@/types/content_metadata"
 
-import article20220218 from "@/routes/articles/different_levels_of_abstraction_in_programming/meta"
-import article20220220 from "@/routes/articles/centering_a_child_element/meta"
-import article20220312 from "@/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta"
 import article20221015 from "@/routes/articles/installation_guide_for_fragmented_web_servers/meta"
+import article20220312 from "@/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta"
+import article20220220 from "@/routes/articles/centering_a_child_element/meta"
+import article20220218 from "@/routes/articles/different_levels_of_abstraction_in_programming/meta"
 
 const metaCollection: ArticlePageMeta[] = [
-	article20220218,
-	article20220220,
+	article20221015,
 	article20220312,
-	article20221015
+	article20220220,
+	article20220218
 ]
 
 export default metaCollection
