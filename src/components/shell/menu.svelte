5e0edf0ed5e31dd3f1d41ecb6dfb6a314379dfe0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-18 23:03:38 +0800

    refactor(shell): update the mode for menu

--- src/components/shell/menu.svelte
+++ src/components/shell/menu.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
-	import { MAIN_CONTENT_ID } from "@/constants/miscellaneous_meta"
+import { MAIN_CONTENT_ID } from "@/constants/miscellaneous_meta"
 
-	import { internalTypes } from "@/components/general/links/constants"
+import { internalTypes } from "@/components/general/links/constants"
 
-	import Icon from "@/components/general/icon.svelte"
-	import BaseLink from "@/components/general/links/base.svelte"
+import Icon from "@/components/general/icon.svelte"
+import BaseLink from "@/components/general/links/base.svelte"
 
-	export let mustShowHorizontally = false
+let {
+	mustShowHorizontally = false
+}: {
+	mustShowHorizontally?: boolean
+} = $props()
 
-	const linkClasses = [ "inline-flex", "flex-row", "flex-nowrap", "items-center", "no-underline" ]
+const linkClasses = [ "inline-flex", "flex-row", "flex-nowrap", "items-center", "no-underline" ]
 </script>
 
 <ul class="menu bg-primary-100" class:menu-horizontal={mustShowHorizontally}>
 	<li>
 		<BaseLink address="/" class={linkClasses} relationship={internalTypes}>
 			<Icon name="home"/>
-			<span class="flex-1">Home<span>
+			<span class="flex-1">Home</span>
 		</BaseLink>
+	</li>
+	<li>
 		<BaseLink address="/projects" class={linkClasses} relationship={internalTypes}>
 			<Icon name="code"/>
-			<span class="flex-1">Projects<span>
+			<span class="flex-1">Projects</span>
 		</BaseLink>
+	</li>
+	<li>
 		<BaseLink address="/articles" class={linkClasses} relationship={internalTypes}>
 			<Icon name="article"/>
-			<span class="flex-1">Articles<span>
+			<span class="flex-1">Articles</span>
 		</BaseLink>
+	</li>
+	<li>
 		<BaseLink
 			address={`/about_myself#${MAIN_CONTENT_ID}`}
 			class={linkClasses}
 			relationship={internalTypes}>
 			<Icon name="badge"/>
-			<span class="flex-1">About myself<span>
+			<span class="flex-1">About myself</span>
 		</BaseLink>
+	</li>
+	<li>
 		<BaseLink address="/website_info" class={linkClasses} relationship={internalTypes}>
 			<Icon name="web"/>
-			<span class="flex-1">Website info<span>
+			<span class="flex-1">Website info</span>
 		</BaseLink>
+	</li>
+	<li>
 		<BaseLink
 			address="/about_myself#contact_details"
 			class={linkClasses}
 			relationship={internalTypes}>
 			<Icon name="contacts"/>
-			<span class="flex-1">Contacts<span>
+			<span class="flex-1">Contacts</span>
 		</BaseLink>
 	</li>
 </ul>
