d2b391dc2f6f1dd2487ab7d47b42f03d7f875507
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-18 23:13:52 +0800

    refactor(shell): update the mode for theme toggler

--- src/components/shell/theme_toggler.svelte
+++ src/components/shell/theme_toggler.svelte
@@ -1,34 +1,41 @@
 <script lang="ts">
-	import { onMount } from "svelte"
-	import { themeChange } from "theme-change"
-	import { DARK_MODE, LIGHT_MODE, mustBeInDarkMode, themeName } from "@/components/general/theme"
+import { onMount } from "svelte"
+import { themeChange } from "theme-change"
+import { DARK_MODE, LIGHT_MODE, mustBeInDarkMode, themeName } from "@/components/general/theme"
 
-	import Icon from "@/components/general/icon.svelte"
-	import toggleBySpace from "@/utilities/toggle_by_space"
+import Icon from "@/components/general/icon.svelte"
+import toggleBySpace from "@/utilities/toggle_by_space"
 
-	$: if (typeof window !== "undefined") {
+$effect(() => {
+	const rawThemeName = $themeName
+	if (typeof window !== "undefined") {
 		document.documentElement.setAttribute(
 			"data-theme",
-			$themeName
+			rawThemeName
 		)
 	}
-	$: modeIcon = $mustBeInDarkMode ? "dark_mode" : "light_mode"
-	$: currentTheme = $mustBeInDarkMode ? DARK_MODE : LIGHT_MODE
+});
+let modeIcon = $derived($mustBeInDarkMode ? "dark_mode" : "light_mode")
+let currentTheme = $derived($mustBeInDarkMode ? DARK_MODE : LIGHT_MODE)
 
-	onMount(() => {
-		themeChange(false)
-		$mustBeInDarkMode = document.documentElement.dataset.theme === DARK_MODE
-	})
+onMount(() => {
+	themeChange(false)
+	$mustBeInDarkMode = document.documentElement
+		.dataset
+		.theme === DARK_MODE
+})
 
-	function toggleThemeThoughMouse(_event: MouseEvent): void {
+function toggleThemeThoughMouse(_event: MouseEvent): void {
+	$mustBeInDarkMode = !$mustBeInDarkMode
+}
+
+function toggleThemeThroughKeyboard(event: KeyboardEvent): void {
+	event.preventDefault()
+	event.stopPropagation()
+	toggleBySpace(event, () => {
 		$mustBeInDarkMode = !$mustBeInDarkMode
-	}
-
-	function toggleThemeThroughKeyboard(event: KeyboardEvent): void {
-		toggleBySpace(event, () => {
-			$mustBeInDarkMode = !$mustBeInDarkMode
-		})
-	}
+	})
+}
 </script>
 
 <button
@@ -38,8 +45,14 @@
 	data-set-theme={currentTheme}
 	data-act-class={LIGHT_MODE}
 	aria-label="Toggle theme"
-	on:keyup|stopPropagation|preventDefault={toggleThemeThroughKeyboard}
-	on:click={toggleThemeThoughMouse}
-	class="btn bg-transparent border-transparent cursor-pointer text-secondary hover:text-primary hover:bg-secondary">
+	onkeyup={toggleThemeThroughKeyboard}
+	onclick={toggleThemeThoughMouse}
+	class="btn bg-transparent border-transparent cursor-pointer hover:primary-content hover:bg-secondary-content link inline-flex flex-row flex-nowrap items-center no-underline font-normal">
 	<Icon name={modeIcon}/>
 </button>
+
+<style lang="postcss">
+.btn {
+	padding-inline: calc(0.25rem * 3);
+}
+</style>
