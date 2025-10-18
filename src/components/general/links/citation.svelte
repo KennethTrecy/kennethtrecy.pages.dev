603fb03590c9eda36c8792c701881772e3fc958b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:10:59 +0800

    refactor(component): update the mode for citation link

--- src/components/general/links/citation.svelte
+++ src/components/general/links/citation.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
-	import { onMount } from "svelte"
+import { type Snippet } from "svelte"
+import { type ReferenceInfo } from "@/types/reference"
 
-	import { type ReferenceInfo } from "@/types/reference"
+import { onMount } from "svelte"
 
-	import { internalTypes } from "@/components/general/links/constants"
+import { internalTypes } from "@/components/general/links/constants"
 
-	import { appendReference } from "@/components/general/containers/reference_info_collection"
+import { appendReference } from "@/components/general/containers/reference_info_collection"
 
-	import BaseLink from "@/components/general/links/base.svelte"
-	import ExternalLink from "@/components/general/links/external.svelte"
+import BaseLink from "@/components/general/links/base.svelte"
+import ExternalLink from "@/components/general/links/external.svelte"
 
-	export let info: ReferenceInfo
-	export let itemprop: string|undefined = undefined
+let { info, itemprop = undefined, children }: {
+	info: ReferenceInfo
+	itemprop?: string|undefined
+	children?: Snippet
+} = $props()
 
-	onMount(() => {
-		appendReference(info)
-	})
+onMount(() => {
+	appendReference(info)
+})
 
-	$: linkComponent = info.linkCategory === "inbound"
-		? BaseLink
-		: ExternalLink
-	$: relationship = info.linkCategory === "inbound"
-		? internalTypes
-		: []
+let linkComponent = $derived(info.linkCategory === "inbound"
+	? BaseLink
+	: ExternalLink)
+let relationship = $derived(info.linkCategory === "inbound"
+	? internalTypes
+	: [])
+
+const SvelteComponent = $derived(linkComponent);
 </script>
 
-<svelte:component
-	this={linkComponent}
+<SvelteComponent
 	address={info.link}
 	{itemprop}
-	{relationship}><slot></slot></svelte:component>
+	{relationship}>{@render children?.()}</SvelteComponent>
