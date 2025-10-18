407b7dc9fc7a387ad70c5f1dcf491971a47a6d8d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:46:40 +0800

    fix(component)!: update the mode for quartenary heading

---
+++ src/components/general/headings/quarternary.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { type Snippet } from "svelte"
+import { type HeadingInfo, type SubheadingVariant } from "@/types/container_info"
+
+import Subheading from "@/components/general/headings/subheading.svelte"
+
+let {
+	headingInfo,
+	variant = "headline",
+	mayUseBookmark = true,
+	class: otherClasses = [],
+	children
+}: {
+	headingInfo: HeadingInfo<"defined">;
+	variant?: SubheadingVariant
+	mayUseBookmark?: boolean
+	class?: string[]
+	children?: Snippet
+} = $props()
+
+let joinedClasses = $derived([
+	...otherClasses
+])
+</script>
+
+<Subheading level={4} {headingInfo} {variant} {mayUseBookmark} class={joinedClasses}>
+	{@render children?.()}
+</Subheading>
