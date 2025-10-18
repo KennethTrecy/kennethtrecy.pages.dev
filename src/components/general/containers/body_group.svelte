49957b688a22d31fa7f4d14a51b3a910d7768b8d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:13:00 +0800

    refactor(component): update the mode for body group container

--- src/components/general/containers/body_group.svelte
+++ src/components/general/containers/body_group.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+import { type Snippet } from "svelte"
+
+let { children }: {
+	children: Snippet
+} = $props()
+</script>
+
 <div itemprop="articleBody">
-	<slot></slot>
+	{@render children()}
 </div>
