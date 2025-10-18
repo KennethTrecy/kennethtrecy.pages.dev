9e557d4fafc511d2b3c912cd5ce4c8ab97a83eea
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:34:49 +0800

    refactor(component): update the mode for example code part

--- src/components/general/containers/example_code_part.svelte
+++ src/components/general/containers/example_code_part.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-	export let filename: string
-	export let rawCodeLines: string[]
-	export let itemprop = "workExample"
-	export let beginLineIndex = 0
-	export let endLineIndex = Infinity
+let {
+	filename,
+	rawCodeLines,
+	itemprop = "workExample",
+	beginLineIndex = 0,
+	endLineIndex = Infinity
+}: {
+	filename: string
+	rawCodeLines: string[]
+	itemprop?: string
+	beginLineIndex?: number
+	endLineIndex?: number
+} = $props()
 
-	$: targetBeginLineIndex = Math.max(0, beginLineIndex)
-	$: targetEndLineIndex = Math.min(rawCodeLines.length, endLineIndex)
-	$: codeLines = rawCodeLines.slice(targetBeginLineIndex, targetEndLineIndex)
+let targetBeginLineIndex = $derived(Math.max(0, beginLineIndex))
+let targetEndLineIndex = $derived(Math.min(rawCodeLines.length, endLineIndex))
+let codeLines = $derived(rawCodeLines.slice(targetBeginLineIndex, targetEndLineIndex))
 </script>
 
-<div class="code_container" {itemprop} itemscope itemtype="https://schema.org/SoftwareSourceCode">
+<div class="code_container lg:max-w-[100vw]" {itemprop} itemscope itemtype="https://schema.org/SoftwareSourceCode">
 	<div class="mockup-code not-prose text-sm">
 		{#each codeLines as line, i}
 			<pre data-prefix={targetBeginLineIndex+i+1}><code>{line}</code></pre>
@@ -22,16 +30,10 @@
 </div>
 
 <style lang="postcss">
-	.code_container {
-		/**
-		 * 3rem is the padding of the general `.container`.
-		 */
-		max-width: calc(100vw - 1rem * 2);
-	}
-
-	@screen lg {
-		.code_container {
-			@apply max-w-[100vw]
-		}
-	}
+.code_container {
+	/**
+		* 3rem is the padding of the general `.container`.
+		*/
+	max-width: calc(100vw - 1rem * 2);
+}
 </style>
