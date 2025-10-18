55a76487be990d2f8f80180d94358b7043c0126f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 01:21:28 +0800

    refactor(component): update the mode for labeled thing container

--- src/components/general/containers/labeled_thing.svelte
+++ src/components/general/containers/labeled_thing.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	export let label: string
+import { type Snippet } from "svelte"
+
+let { label, children }: {
+	label: string
+	children: Snippet
+} = $props()
 </script>
 
 <span itemprop="item" itemscope itemtype="https://schema.org/Thing">
 	<strong itemprop="name">{label}</strong>.
-	<span itemprop="description"><slot></slot></span>
+	<span itemprop="description">{@render children()}</span>
 </span>
