96f8876a1bf4e59984947813f67eff3a2327cf86
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-10-19 00:23:16 +0800

    refactor(component): update the mode for brand icon

--- src/components/general/brand_icon.svelte
+++ src/components/general/brand_icon.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-	import { textColor } from "@/components/general/theme"
+import { textColor } from "@/components/general/theme"
 
-	export let icon: string
-	export let alternateName: string
-	$: link = `https://cdn.simpleicons.org/${icon}/${$textColor}`
+let {
+	icon,
+	alternateName
+}: {
+	icon: string
+	alternateName: string
+} = $props()
+let link = $derived(`https://cdn.simpleicons.org/${icon}/${$textColor}`)
 </script>
 
 <img class="h-8 w-auto" alt={alternateName} width="32" height="32" src={link}/>
