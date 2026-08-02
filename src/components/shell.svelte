e49434fd3c20ba095c604cea64948f0f0461eaee
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2026-08-02 11:47:01 +0800

    docs: update copyright year

--- src/components/shell.svelte
+++ src/components/shell.svelte
@@ -99,7 +99,7 @@ function toggleMenuThroughKeyboard(event: KeyboardEvent): void {
 			<section class="grid-flow-col items-center">
 				<img alt="logo" src={Logo} width="64" height="64" class="flex-none w-16 h-auto"/>
 				<SimpleText itemprop="copyrightNotice">
-					Copyright © <span itemprop="copyrightYear">2025</span>
+					Copyright © <span itemprop="copyrightYear">2026</span>
 					<span itemprop="copyrightHolder">Kenneth Trecy Tobias</span>.
 				</SimpleText>
 			</section>
